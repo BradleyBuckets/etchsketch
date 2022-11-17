f9aca724daca3f42d48ce2d315b13968c39fceff
Bradley Griffin <bigriff11@gmail.com>
2022-11-17 16:57:52 -0700

    Add functions for seperation of concerns and allow the user to set the grid side length

--- etchsketch.js
+++ etchsketch.js
@@ -1,15 +1,38 @@
 let grid = document.querySelector('.container')
-
 let gridSide = 16
-document.documentElement.style.setProperty('--gridLength', gridSide);
 
-for (let i = 0; i < (gridSide * gridSide); i++) {
-    let div = document.createElement('div');
-    div.addEventListener('mouseover', backgroundToggle)
-    div.classList.add(`div-${i}`);
-    grid.appendChild(div);
+createGrid()
+
+function createGrid() {
+    document.documentElement.style.setProperty('--gridLength', gridSide);
+    for (let i = 0; i < (gridSide * gridSide); i++) {
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', blackBackround)
+        div.classList.add(`div-${i}`);
+        div.classList.add(`div`);
+        grid.appendChild(div);
+    }
 }
 
-function backgroundToggle() {
+function blackBackround() {
     this.classList.add('black')
+}
+
+let button = document.querySelector('.button')
+button.addEventListener('click', setGridSide)
+function setGridSide() {
+    clearBoard();
+    let request = prompt('Please type in the size of one side of your grid', 16);
+    let number = parseInt(request)
+    if (number > 100) number = 100;
+    if (number < 10) number = 10;
+    gridSide = number;
+    createGrid();
+}
+
+function clearBoard(){
+    let squares = document.querySelectorAll('.div');
+    for (let square of squares) {
+        square.classList.remove('black');
+    }
 }
