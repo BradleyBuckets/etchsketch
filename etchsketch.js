40dc780ce2b861733dfb514c3a12444edffb3863
Bradley Griffin <bigriff11@gmail.com>
2022-11-17 08:58:14 -0700

    Create function to apply black class and corresponding background color

--- etchsketch.js
+++ etchsketch.js
@@ -1,7 +1,12 @@
 let grid = document.querySelector('.container')
 
-for (let i = 0; i < 64; i++) {
+for (let i = 0; i < (16 * 16); i++) {
     let div = document.createElement('div');
+    div.addEventListener('mouseover', backgroundToggle)
     div.classList.add(`div-${i}`);
     grid.appendChild(div);
+}
+
+function backgroundToggle() {
+    this.classList.add('black')
 }
