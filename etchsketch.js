690883e2018fd2206fbd647f5f848708b096e115
Bradley Griffin <bigriff11@gmail.com>
2022-11-17 17:23:22 -0700

    Fix resizing bug

--- etchsketch.js
+++ etchsketch.js
@@ -1,13 +1,13 @@
 let grid = document.querySelector('.container')
-let gridSide = 16
+let gridSize = 16
 
-createGrid()
+createGrid(blackBackround, gridSize)
 
-function createGrid() {
+function createGrid(background, gridSide) {
     document.documentElement.style.setProperty('--gridLength', gridSide);
     for (let i = 0; i < (gridSide * gridSide); i++) {
         let div = document.createElement('div');
-        div.addEventListener('mouseover', blackBackround)
+        div.addEventListener('mouseover', background)
         div.classList.add(`div-${i}`);
         div.classList.add(`div`);
         grid.appendChild(div);
@@ -17,6 +17,9 @@ function createGrid() {
 function blackBackround() {
     this.classList.add('black')
 }
+function blueBackround() {
+    this.classList.add('blue')
+}
 
 let button = document.querySelector('.button')
 button.addEventListener('click', setGridSide)
@@ -24,10 +27,13 @@ function setGridSide() {
     clearBoard();
     let request = prompt('Please type in the size of one side of your grid', 16);
     let number = parseInt(request)
-    if (number > 100) number = 100;
-    if (number < 10) number = 10;
-    gridSide = number;
-    createGrid();
+    if (number > 100) {
+        number = 100;
+    }  
+    if (number < 10) {
+        number = 10;
+    }
+    createGrid(blackBackround, number);
 }
 
 function clearBoard(){
